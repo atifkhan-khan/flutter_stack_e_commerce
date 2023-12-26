dfebf85bb0896feba8da8d2fb6a94b1223a2a758
Atifkhan1122 <atifk7848@gmail.com>
2023-12-26 12:07:54 -0800

    - DashBoard UI Design
    Signed-off-by: Atifkhan1122 <atifk7848@gmail.com>

--- lib/ui/views/home/home_view.dart
+++ lib/ui/views/home/home_view.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
-import 'package:flutter_stack_e_comerce/ui/common/app_colors.dart';
 import 'package:flutter_stack_e_comerce/ui/common/ui_helpers.dart';
-
+import 'package:intrinsic_grid_view/intrinsic_grid_view.dart';
 import 'home_viewmodel.dart';
 
 class HomeView extends StackedView<HomeViewModel> {
@@ -19,59 +18,38 @@ class HomeView extends StackedView<HomeViewModel> {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 25.0),
           child: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                verticalSpaceLarge,
-                Column(
+              child: Column(
+            children: [
+              verticalSpaceMedium,
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(22)),
+                height: 35,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    const Text(
-                      'Hello, STACKED!',
-                      style: TextStyle(
-                        fontSize: 35,
-                        fontWeight: FontWeight.w900,
-                      ),
-                    ),
-                    verticalSpaceMedium,
-                    MaterialButton(
-                      color: Colors.black,
-                      onPressed: viewModel.incrementCounter,
-                      child: Text(
-                        viewModel.counterLabel,
-                        style: const TextStyle(color: Colors.white),
-                      ),
+                    Padding(
+                        padding: EdgeInsets.only(left: 8),
+                        child: Icon(Icons.search)),
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Padding(
+                            padding: EdgeInsets.only(right: 8),
+                            child: Icon(Icons.favorite_border_outlined)),
+                        horizontalSpaceSmall,
+                        Padding(
+                            padding: EdgeInsets.only(right: 8),
+                            child: Icon(Icons.shopping_bag_outlined))
+                      ],
                     ),
                   ],
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    MaterialButton(
-                      color: kcDarkGreyColor,
-                      onPressed: viewModel.showDialog,
-                      child: const Text(
-                        'Show Dialog',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                    MaterialButton(
-                      color: kcDarkGreyColor,
-                      onPressed: viewModel.showBottomSheet,
-                      child: const Text(
-                        'Show Bottom Sheet',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ],
-                )
-              ],
-            ),
-          ),
+              ),
+              ProductsListWidget()
+            ],
+          )),
         ),
       ),
     );
@@ -82,4 +60,63 @@ class HomeView extends StackedView<HomeViewModel> {
     BuildContext context,
   ) =>
       HomeViewModel();
+}
+
+Widget ProductsListWidget() {
+  return Expanded(
+    child: Container(
+        child: IntrinsicGridView.vertical(
+            verticalSpace: 10,
+            horizontalSpace: 10,
+            padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+            //shrinkWrap: true,
+            children: List.generate(112, (index) {
+              return Card(
+                  clipBehavior: Clip.antiAlias,
+
+                  // margin: EdgeInsetsDirectional.all(8),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                          child: Stack(
+                        alignment: Alignment.topLeft,
+                        children: [
+                          SizedBox(
+                            height: 120,
+                            width: 170,
+                            child: Image.network(
+                                height: 120,
+                                width: 170,
+                                fit: BoxFit.fill,
+                                "https://hornblower-businesses.co.uk/wp-content/uploads/2020/02/electrical-equipment-manufacturer_1000X750.jpg"),
+                          ),
+                          IconButton(
+                              onPressed: () {}, icon: Icon(Icons.favorite)),
+                        ],
+                      )),
+                      verticalSpaceTiny,
+                      Padding(
+                        padding: const EdgeInsets.only(left: 5),
+                        child: Text(
+                          "Product Name",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      verticalSpaceTiny,
+                      Padding(
+                        padding: const EdgeInsets.only(left: 5),
+                        child: Text(
+                          "Price 12£",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      verticalSpaceSmall,
+                    ],
+                  ));
+            }))),
+  );
 }
